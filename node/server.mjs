cbddd0e16029d68d75c518bce00c853b70d9219d
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-10-08 21:39:42 +0100

    post request

--- node/server.mjs
+++ node/server.mjs
@@ -1,11 +1,11 @@
 import express from 'express';
 
 const app = express();
-app.use((req, res, next)=>{
+/*app.use((req, res, next)=>{
   console.log(<h1>hello</h1>)
   // it doesn't go on until it sees the next method
   next();
-})
+})*/
 
 app.get('/profile', (req, res) =>{
   res.send("profile url");
@@ -19,3 +19,11 @@ app.get('/', (req, res) =>{
   res.send(user);
 });
 app.listen(3000);
+
+
+/* app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+If you are using Express 4.16+ you can now replace that with:
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());*/
